36d451c1b07053ca8682203ed96107b7689bbd4b
Shyam Barange <shyam.barange0311@gmail.com>
2018-12-07 16:38:19 +0530

    Third commit

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -10,12 +10,3 @@ Java_com_xplorazzi_xplorazzilibraries_MainActivity_stringFromJNI(
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
-
-
-extern "C"
-JNIEXPORT jstring JNICALL
-Java_com_xplorazzi_xpztoasterlibrary_XpzToast_stringFromJNI( JNIEnv *env,
-                                                             jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
-}
--- xpztoasterlibrary/src/main/java/com/xplorazzi/xpztoasterlibrary/XpzToast.java
+++ xpztoasterlibrary/src/main/java/com/xplorazzi/xpztoasterlibrary/XpzToast.java
@@ -5,13 +5,13 @@ import android.widget.Toast;
 
 public class XpzToast {
 
-    public static void XpzToastText(Context c, String message){
+    public void XpzToastText(Context c, String message){
 
         Toast.makeText(c,stringFromJNI()+ "  "+ message,Toast.LENGTH_SHORT).show();
 
     }
 
-    public static native String stringFromJNI();
+    public native String stringFromJNI();
 
 
 }
