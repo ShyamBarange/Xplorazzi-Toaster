af1e34cf6b1fabe551ce54387abd9c4b924ed1ee
Shyam Barange <shyam.barange0311@gmail.com>
2018-12-07 15:33:17 +0530

    Initial commit

---
+++ xpztoasterlibrary/src/main/java/com/xplorazzi/xpztoasterlibrary/XpzToast.java
@@ -0,0 +1,13 @@
+package com.xplorazzi.xpztoasterlibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class XpzToast {
+
+    public static void XpzToastText(Context c, String message){
+
+        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+    }
+
+}
