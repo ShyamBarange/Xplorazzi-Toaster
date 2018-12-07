b119d5808b93ee15abfa7cfb7de485e73c2d63f3
Shyam Barange <shyam.barange0311@gmail.com>
2018-12-07 17:08:59 +0530

    final commit

--- xpztoasterlibrary/src/main/java/com/xplorazzi/xpztoasterlibrary/XpzToast.java
+++ xpztoasterlibrary/src/main/java/com/xplorazzi/xpztoasterlibrary/XpzToast.java
@@ -5,14 +5,11 @@ import android.widget.Toast;
 
 public class XpzToast {
 
-    public static void XpzToastText(Context c, String message){
+    public static void XpzToastText(Context c, String message) {
 
-        Toast.makeText(c,stringFromJNI()+ "  "+ message,Toast.LENGTH_SHORT).show();
+        Toast.makeText(c, message, Toast.LENGTH_SHORT).show();
 
     }
-
-    // linked with cpp code..
-    public static native String stringFromJNI();
 
 
 }
