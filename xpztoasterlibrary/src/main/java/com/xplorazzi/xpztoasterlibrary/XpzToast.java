9f4c051833903ba61915c835c05001a5f208be42
Shyam Barange <shyam.barange0311@gmail.com>
2018-12-07 16:25:06 +0530

    second commit..

--- xpztoasterlibrary/src/main/java/com/xplorazzi/xpztoasterlibrary/XpzToast.java
+++ xpztoasterlibrary/src/main/java/com/xplorazzi/xpztoasterlibrary/XpzToast.java
@@ -7,7 +7,11 @@ public class XpzToast {
 
     public static void XpzToastText(Context c, String message){
 
-        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+        Toast.makeText(c,stringFromJNI()+ "  "+ message,Toast.LENGTH_SHORT).show();
+
     }
+
+    public static native String stringFromJNI();
+
 
 }
